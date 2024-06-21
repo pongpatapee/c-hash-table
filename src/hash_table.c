e9676b1fd9f6a48b62fc33d017bc81b33d89e470
pongpatapee <pongpatapee1021@gmail.com>
2024-06-21 10:59:07 -0400

    feat: implemented hash table API methods

--- src/hash_table.c
+++ src/hash_table.c
@@ -8,6 +8,8 @@
 #define HT_PRIME1 151
 #define HT_PRIME2 163
 
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
 // static in C means only visible in the current file
 static ht_item* ht_new_item(const char* k, const char* v) {
   ht_item* i = malloc(sizeof(ht_item));
@@ -98,16 +100,112 @@ static int ht_get_hash(const char* s, const int num_buckets,
   return (hash_a + attempt * (hash_b + 1)) % num_buckets;
 }
 
+// API methods for hash table
+void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
+  ht_item* item = ht_new_item(key, value);
+  int attempts = 0;
+  int index = ht_get_hash(key, ht->size, attempts);
+  ht_item* curr_item = ht->items[index];
+
+  while (curr_item != NULL) {
+    if (curr_item != &HT_DELETED_ITEM) {
+      // Update value if key already exists
+      if (strcmp(curr_item->key, key) == 0) {
+        ht_del_item(curr_item);
+        ht->items[index] = item;
+        return;
+      }
+    }
+    index = ht_get_hash(key, ht->size, ++attempts);
+  }
+
+  ht->items[index] = item;
+  ht->count++;
+}
+
+char* ht_search(ht_hash_table* ht, const char* key) {
+  int attempts = 0;
+  int index = ht_get_hash(key, ht->size, attempts);
+  ht_item* item = ht->items[index];
+  while (item != NULL) {
+    if (item != &HT_DELETED_ITEM) {
+      if (strcmp(item->key, key) == 0) {
+        return item->value;
+      }
+    }
+    index = ht_get_hash(key, ht->size, ++attempts);
+    item = ht->items[index];
+  }
+
+  return NULL;
+}
+
+/*
+Deletion is more complicated than insertion and seraching
+Since the item we're deleting may be part of a collision chain.
+
+If we mark something as NULL, it might make keys at the tail of the
+collision chain impossible to find.
+
+Instead we will simply mark the item as deleted using HT_DELETED_ITEM
+*/
+
+void ht_delete(ht_hash_table* ht, const char* key) {
+  int attempts = 0;
+  int index = ht_get_hash(key, ht->size, attempts);
+  ht_item* item = ht->items[index];
+  while (item != NULL) {
+    if (item != &HT_DELETED_ITEM) {
+      if (strcmp(item->key, key) == 0) {
+        ht_del_item(item);
+        ht->items[index] = &HT_DELETED_ITEM;
+      }
+    }
+    index = ht_get_hash(key, ht->size, ++attempts);
+    item = ht->items[index];
+  }
+  ht->count--;
+}
+
+// My custom print functions
+void print_ht_item(ht_item* item) {
+  printf("k: %s, v: %s\n", item->key, item->value);
+}
+
+void print_ht_hash_table(ht_hash_table* ht) {
+  printf("Hash Table: \n");
+  printf("size: %d, count: %d, items:\n", ht->size, ht->count);
+  for (int i = 0; i < ht->size; i++) {
+    if (ht->items[i] == NULL || ht->items[i] == &HT_DELETED_ITEM)
+      continue;
+    printf("\tindex: %d, ", i);
+    print_ht_item(ht->items[i]);
+  }
+}
+
 int main() {
   printf("C hash table\n");
+  ht_hash_table* ht = ht_new();
+  ht_insert(ht, "1", "value1");
+  ht_insert(ht, "2", "value2");
+  ht_insert(ht, "some_key", "some value");
+  ht_insert(ht, "joe", "mama");
+  print_ht_hash_table(ht);
 
-  // ht_hash_table* ht = ht_new();
-  // printf("%p\n", ht);
-  // ht_del_hash_table(ht);
-  printf("Calling ht_get_hash\n");
-  char* key = "some random key";
-  int hash = ht_get_hash(key, 40, 10);
-  printf("Key: %s, Hash: %d\n", key, hash);
+  char* search_key = "some_key";
+  printf("\nsearching for key: %s\n", search_key);
+  char* val = ht_search(ht, search_key);
+  printf("Value found: %s\n", val);
+
+  char* key_to_del = "joe";
+  printf("\ndeleting key: %s\n", key_to_del);
+  ht_delete(ht, key_to_del);
+  print_ht_hash_table(ht);
+
+  char* key_to_update = "2";
+  printf("\nupdating value for key: %s\n", key_to_update);
+  ht_insert(ht, key_to_update, "new value2");
+  print_ht_hash_table(ht);
 
   return 0;
 }
--- src/hash_table.h
+++ src/hash_table.h
@@ -11,3 +11,12 @@ typedef struct {
   ht_item** items; // Double pointers because we want an Array to item pointers
 
 } ht_hash_table;
+
+// API for hash_table
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
+
+// My custom print functions
+void print_ht_item(ht_item* item);
+void print_ht_hash_table(ht_hash_table* ht);
